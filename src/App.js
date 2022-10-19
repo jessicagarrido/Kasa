b996928b54e55c076571fc3c723930f582dde9c1
Jessica Garrido <jessica.garrido.di@gmail.com>
2022-10-19 22:30:05 +0200

    Création de la page erreur 404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './scenes/Home';
 import About from './components/About';
 import Gallery from './scenes/Gallery';
 import Footer from './components/Footer';
+import ErrorPage from './components/ErrorPage';
 
 import {Routes, Route} from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
        <Footer />
      </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,52 @@
-
-import bannerAbout from '../assets/img/Banniere-apropos.png'
+import bannerAbout from "../assets/img/Banniere-apropos.png";
 
 function About() {
   return (
     <section class="sectionBanner">
-      <div className='bannerAbout' id='bannerAbout'>
+      <div className="bannerAbout" id="bannerAbout">
         <img src={bannerAbout} alt="Image de montagnes" />
       </div>
-    </section>
-    
 
-     
+      <div class="aboutTextContener">
+        <div>
+          <h1>Fiabilité</h1>
+          
+          <p>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </p>
+        </div>
+        <div>
+          <h1>Respect</h1>
+          <p>
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </p>
+        </div>
+        <div>
+          <h1>Service</h1>
+          <p>
+            Nos équipes se tiennent à votre disposition pour vous fournir une
+            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+            moindre question.
+          </p>
+        </div>
+        <div>
+          <h1>Responsabilité</h1>
+          <p>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+            les standards sont bien respectés. Nous organisons également des
+            ateliers sur la sécurité domestique pour nos hôtes.
+          </p>
+        </div>
+      </div>
+    </section>
   );
-};
-  
-  export default About;
+}
+
+export default About;
